0ab0750c3d9f21302a9c81b10d194967a1d114e5
Desenvolvimento <airtonluizcanalefilho@hotmail.com@hotmail.com>
2023-01-19 10:28:39 -0300

    feat: icon and deploy

--- README.md
+++ README.md
@@ -1,1 +1,70 @@
-not started yet
+<h1 align="center"><strong></strong></h1>
+<h1 align="center"><strong>Pinterest Page</strong></h1>
+<br>
+<p align="center">
+  <a href="#-project">Project</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#installation">Installation</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-technologies">Technologies</a>
+  &nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-deploy">Deploy</a>
+</p>
+
+<br>
+
+
+
+### This is a project that i practice my knowledge of ReactJS and TailwindCSS using Phosphor icons library.
+
+<br>
+
+## 💻 Project
+
+
+<p>This app shows a page as similar as possible to a pinterest template.</p>
+
+<br>
+<img src="./github/readme.gif" alt="Readme Gif">
+<br>
+<br>
+
+## 🚀 Deploy
+ -  https://pinterest-page.canale.dev
+
+ - This application was deployed with Github Pages library 
+
+
+ ```bash
+ 
+# Running deploy
+$ npm run deploy
+```
+<br>
+
+## Installation
+
+- Important: You must have installed the React.js and Git
+
+```bash
+# clone it
+$ git clone git@github.com:airton-canale/pinterest-page.git
+
+$ cd pinterest-page
+
+# Install dependencies
+$ npm install
+
+# Running scripts
+$ npm start
+```
+- And enter in your browser using http://localhost:3000
+
+
+## 🚀 Technologies
+
+This project was developed using the following technologies:
+
+- <img height="30" src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/react/react.png"> **React.JS**
+- <img height="30" src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/tailwind/tailwind.png"> **Tailwind**
+- <img height="30" src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/javascript/javascript.png"> **Javascript**
+- <img height="30" src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/css/css.png"> **CSS**
+
--- src/components/StartupDesigned.jsx
+++ src/components/StartupDesigned.jsx
@@ -1,21 +1,46 @@
+import { CheckSquare } from "phosphor-react";
 import React from "react";
 import ButtonWatchIntro from "./ButtonWatchIntro";
 
-const StartupDesined = ({inverted}) => {
-
-    
+const StartupDesined = ({ inverted }) => {
   return (
     <div className="grid grid-cols-2 gap-12 p-20 bg-[#f3f3f3]">
       <div className="w-full">
-        <img className="w-full mix-blend-multiply -my-40" src="/assets/design.webp" alt="a" />
+        <img
+          className="w-full mix-blend-multiply -my-40"
+          src="/assets/design.webp"
+          alt="a"
+        />
       </div>
-      <div className={`flex flex-col h-full justify-center ${inverted ? "row-[1]" : "row-[0]"}`} >
-        <h1>Destiny isn`t A.I, its result of human intelligence.</h1>
-        <p>
-          No home mel, eu persecuti, id ius mutat gubergreen, eros harum
-          henderit ex eos, in qua vocibus inimics gubergren
-        </p>
-        <ButtonWatchIntro />
+      <div
+        className={`flex flex-col h-full justify-center ${
+          inverted ? "row-[1]" : "row-[0]"
+        }`}
+      >
+        <div className="flex flex-col gap-4">
+          <div>
+            <h1>Destiny isn`t A.I, its result of human intelligence.</h1>
+            <p>
+              No home mel, eu persecuti, id ius mutat gubergreen, eros harum
+              henderit ex eos, in qua vocibus inimics gubergren
+            </p>
+          </div>
+          <div className="flex flex-col justify-start m-0">
+            <div className="flex justify-start items-center m-0">
+              <CheckSquare className="m-0 mr-1" size={10} />
+              <p className="">Write some features here.</p>
+            </div>
+            <div className="flex justify-start items-center m-0">
+              <CheckSquare className="m-0 mr-1" size={10} />
+              <p>Repat the same with another one.</p>
+            </div>
+            <div className="flex justify-start items-center m-0">
+              <CheckSquare className="m-0 mr-1" size={10} />
+              <p>Write some features here.</p>
+            </div>
+          </div>
+          <ButtonWatchIntro />
+        </div>
       </div>
     </div>
   );
